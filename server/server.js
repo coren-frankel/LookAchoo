e055894ca13bbd4a0ad684d7ef3bec707a2fd108
unclebabykern <coren.frankel@gmail.com>
2022-09-22 22:47:19 -0600

    Push Updates to directory structure

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8000;
-require('./server/config/mongoose.config');
+require('../server/config/mongoose.config');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-require('./server/routes/sniffle.routes')(app);
+require('../server/routes/sniffle.routes')(app);
 app.listen(port, () => console.log(`Port ${port} is ready for ya, hon...`))
