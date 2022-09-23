3f473b508becaa3ee251b370ec73470109f12405
unclebabykern <coren.frankel@gmail.com>
2022-09-22 20:39:57 -0600

    Updateing before adding client

--- server/routes/sniffle.routes.js
+++ server/routes/sniffle.routes.js
@@ -3,6 +3,6 @@ module.exports = function(app){
     app.get('/api/hello', SniffleController.index)
     app.post('/api/sniffle/new', SniffleController.logNewSniffle);
     app.get('/api/sniffle/:id', SniffleController.getSniffle)
+    //FETCH RANDOM SNIFFLES
     // app.get('/api/sniffle/random', SniffleController.displaySomeSniffles)
-    
 }
