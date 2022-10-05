510176737e07d20bb2543f3b6e66b62837eb5261
unclebabykern <coren.frankel@gmail.com>
2022-10-05 16:03:52 -0600

    Server adjustments to connect express to front-end

--- server/server.js
+++ server/server.js
@@ -1,30 +1,35 @@
-// if(process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config({path: __dirname+'/.env'});
-// }
+// require('dotenv').config
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: __dirname+'/.env'});
+}
 
-require('dotenv').config
 const express = require('express');
 const app = express();
 // const mongoose = require('mongoose')
 const cors = require('cors');
 const logger = require('morgan');
 const path = require('path');
-const port = process.env.PORT || 8000;
 // require('./server/config/mongoose.config');
-app.unsubscribe(logger('dev'))
+app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/api', (req, res) => {
+app.get('/api/test', (req, res) => {
     res.send("testing...")
 })
 // app.use('/api', require('../server/routes/sniffle.routes'));
 //EXPRESS VERCEL SERVERLESS CONNECTION TO CLIENT
 
 
-//SOCKET.io SERVERSIDE FOUNDATION 
-// const  server = 
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (_, res) => {
+    res.sendFile(
+        path.join(__dirname, '../client/build/index.html'));
+        (err) => { if(err) { res.status(500).send(err)}}
+})
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
 
 //ROUTES TO CONNECT FRONTEND & BACKEND
@@ -35,12 +40,6 @@ app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
 
 //STATIC FILES (FRONTEND BUILD)
 // if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (_, res) => {
-        res.sendFile(
-            path.join(__dirname, './client/build/index.html'));
-            (err) => { if(err) { res.status(500).send(err)}}
-    })
 // }
 
 
