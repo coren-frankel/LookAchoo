fbb88cbd714463fbf71ebaeb255aebb282378203
unclebabykern <coren.frankel@gmail.com>
2022-10-05 17:21:01 -0600

    Backtracked to functional frontend and re-build
    Undid the chaos of several incomplete tutorials. Need to research more about hosting back end.

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,10 +1,10 @@
-require('dotenv').config
+// require('dotenv').config
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/achoo_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    // // useCreateIndex: true,
+    // // useFindAndModify: false,
 })
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database', err));
--- server/server.js
+++ server/server.js
@@ -1,53 +1,14 @@
-// require('dotenv').config
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path: __dirname+'/.env'});
-}
-
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose')
 const cors = require('cors');
 const logger = require('morgan');
-const path = require('path');
-// require('./server/config/mongoose.config');
+const port = 8000;
+require('./config/mongoose.config');
 app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/api/test', (req, res) => {
-    res.send("testing...")
-})
-// app.use('/api', require('../server/routes/sniffle.routes'));
-//EXPRESS VERCEL SERVERLESS CONNECTION TO CLIENT
+require('../server/routes/sniffle.routes')(app);
 
-
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('*', (_, res) => {
-//     res.sendFile(
-//         path.join(__dirname, '../client/build/index.html'));
-//         (err) => { if(err) { res.status(500).send(err)}}
-// })
-
-const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
-
-//ROUTES TO CONNECT FRONTEND & BACKEND
-// app.use('/api/hello', require(path.join(__dirname, 'routes', 'sniffle.routes.js')))
-// app.use('/api/sniffle/new', require(path.join(__dirname, 'routes', 'sniffle.routes.js')))
-// app.use('/api/sniffle/random', require(path.join(__dirname, 'routes', 'sniffle.routes.js')))
-// app.use('/api/sniffle/:id', require(path.join(__dirname, 'routes', 'sniffle.routes.js')))
-
-//STATIC FILES (FRONTEND BUILD)
-// if (process.env.NODE_ENV === 'production') {
-// }
-
-
-// const io = require('socket.io')(server, {cors: true});
-// io.on("connection", socket => {
-//     console.log(socket.id)
-//     socket.on("event_from_client", data => {
-//         socket.broadcast.emit("send_data_to_all_other_clients", data);
-//     });
-// });
-// module.exports = app
