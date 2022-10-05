8fc29bf750133f48214ae4f11812eeffff837f3e
unclebabykern <coren.frankel@gmail.com>
2022-10-05 01:27:50 -0600

    Footer adjustment
    tucked under loaded window for scroll reveal

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     }
     return (
         <>
-        <Card elevation={3} sx={{ bgcolor: '#283f3b', color: '#99ddc8', display: 'inline-block', position: 'sticky', bottom: 0, padding: '0 10px'}}>
+        <Card elevation={3} sx={{ bgcolor: '#283f3b', color: '#99ddc8',  position: 'static', bottom: -30, margin: '5% 0', padding: '0 10px', zIndex: -1}}>
             <small style={{}}>
                 <a href="https://github.com/coren-frankel" rel="Coren Frankel's github" style={style.links}>coren frankel</a> 2022<br/>
                 Imagery from <a href="https://icons8.com" style={style.links}>icons8.com</a> | Video from <a href="https://www.pexels.com/" style={style.links}>pexels.com</a>
