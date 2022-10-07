a15f9286ba2683c23ef78bffee27f2b6e0d38ca5
unclebabykern <coren.frankel@gmail.com>
2022-10-06 23:30:08 -0600

    Cleanup package.json

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const path = require('path');
 const port = 8000;
 require('./config/mongoose.config')
 app.use(express.json());
