c3f9bc09547609c87fbcdcb89c97a2ef29deee68
unclebabykern <coren.frankel@gmail.com>
2022-10-10 11:24:00 -0600

    Adjust Cors headers and proxy/homepage urls

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const port = 8000;
 require('./config/mongoose.config')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
-    next();
-});
+const whitelist = [process.env.CLIENT_URL, "http://localhost:3000"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin))
+            return callback(null, true)
+        callback(new Error('Not allowed by CORS'));
+    }
+}
+app.use(cors(corsOptions));
 require('./routes/sniffle.routes')(app);
 
 app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
