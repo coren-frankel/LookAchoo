20d239659b34bdcdda59c9ccc8aadff896a222e5
unclebabykern <coren.frankel@gmail.com>
2022-10-10 10:31:00 -0600

    Adding CORS proxy and request origin server side

--- server/server.js
+++ server/server.js
@@ -5,7 +5,17 @@ const port = 8000;
 require('./config/mongoose.config')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const whitelist = ['https://look-achoo.vercel.app/']
+const corsOptions = {
+    origin: function( origin, callback ) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("CORS isn't having it!"))
+        }
+    }
+}
+app.use(cors(corsOptions));
 require('./routes/sniffle.routes')(app);
 
 app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
