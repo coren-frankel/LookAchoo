4c7b224312d15469da86e182a0f262420f830927
unclebabykern <coren.frankel@gmail.com>
2022-10-10 11:56:49 -0600

    Adapt Cors options

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 8000;
+const port = process.env.PORT || 8000;
 require('./config/mongoose.config')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const whitelist = [process.env.CLIENT_URL, "http://localhost:3000"];
 const corsOptions = {
-    credentials: false,
     origin: (origin, callback) => {
         if (whitelist.includes(origin))
             return callback(null, true)
         callback(new Error('Not allowed by CORS'));
-    }
+    },
+    credentials: true,
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 require('./routes/sniffle.routes')(app);
