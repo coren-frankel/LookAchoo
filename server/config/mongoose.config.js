a0f30dc874c1be2e4fec8f877d236b3c159fec47
unclebabykern <coren.frankel@gmail.com>
2022-10-11 14:43:00 -0600

    Remove unassigned options variable

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, options)
+mongoose.connect(uri)
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database', err));
