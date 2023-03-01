eb85d350cf577792ba03f0ea2d11b6bdbc054041
unclebabykern <coren.frankel@gmail.com>
2023-03-01 15:39:01 -0700

    style changes for code readability

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/achoo_db';
-mongoose.connect(uri).then(() => console.log('Established a connection to the database'))
+mongoose.connect(uri)
+    .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database', err));
--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ require('./config/mongoose.config')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // IF THE CLIENT URL IS FOUND IN ENV VARIABLES
-process.env.CLIENT_URL 
+process.env.CLIENT_URL ?
 // THEN CORS SETTINGS FOR PRODUCTION ENVIRONMENT
-? app.use(cors({
-    origin: process.env.CLIENT_URL,
-    preflightContinue: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials:true
-})) 
-// ELSE, IN DEV ENVIRONMENT, NO CORS SETTINGS NECESSARY
-: app.use(cors());
+    app.use(cors({
+        origin: process.env.CLIENT_URL,
+        preflightContinue: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials:true
+    })) 
+    // ELSE, IN DEV ENVIRONMENT, NO CORS SETTINGS NECESSARY
+    : app.use(cors());
 
 require('./routes/sniffle.routes')(app);
 app.listen(port, () => console.log(`Port ${port} is ready for ya, sweetums...`))
